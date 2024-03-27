327835cfeddc7ff784d17afa41ca58eade6ee774
HectorVillavicencio <fvillavicencio2@gmail.com>
2024-03-27 09:32:07 -0300

    agregue el componente product detail

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -4,11 +4,12 @@ import { match } from 'assert';
 import { Product } from '@shared/models/product.model';
 import {ReversePipe} from '@shared/pipes/reverse.pipe'
 import {TimeAgoPipe} from '@shared/pipes/time-ago.pipe';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule,ReversePipe,TimeAgoPipe],
+  imports: [CommonModule,ReversePipe,TimeAgoPipe,RouterLinkWithHref],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
