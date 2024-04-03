c3ba5ecf4570a730461af11b6034177a2d7a799f
HectorVillavicencio <fvillavicencio2@gmail.com>
2024-04-02 23:04:24 -0300

    agregue el footer

--- src/app/domains/shared/components/layout/layout.component.ts
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule,FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
